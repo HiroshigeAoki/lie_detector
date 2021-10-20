1038620e56e34df32b2452fcaab96fe15c1c80bd
Aoki Hiroshige <aoki.hiroshige.18@shizuoka.ac.jp>
2021-10-20 21:54:11 +0900

    modified

--- train.py
+++ train.py
@@ -18,73 +18,70 @@ warnings.filterwarnings("ignore")
 
 @hydra.main(config_path="config", config_name="defaults")
 def main(cfg: DictConfig) -> None:
-    gmail_sender = Gmailsender(subject=f"Execution end notification (model:{cfg.model.name}, data:{cfg.data.name})")
-
-    pl.seed_everything(1234)
-    logger.info("\n" + OmegaConf.to_yaml(cfg))
-
-    if cfg.model.name == 'HAN':
-        tokenizer = hydra.utils.instantiate(
-            cfg.model.tokenizer,
-            data_dir=cfg.data.dir,
-        )
-
-        data_module = hydra.utils.instantiate(
-            cfg.model.data_module,
-            data_dir=cfg.data.dir,
-            tokenizer=tokenizer,
-        )
-
-        model = hydra.utils.instantiate(
-            cfg.model.general,
-            optim=cfg.optim,
-            embedding_matrix=tokenizer.embedding_matrix,
-            _recursive_=False,
-        )
-
-    elif cfg.model.name=='HierBERT' or cfg.model.name=='HierRoBERT':
-        # TODO: 色々書く。
-        data_module = hydra.utils.instantiate(
-            cfg.model.data_module,
-            data_dir=cfg.data.dir,
-            tokenizer=cfg.model.tokenizer,
-            _recursive_=False,
-        )
-
-        model = hydra.utils.instantiate(
-            cfg.model.model,
-            pretrained_model=cfg.model.tokenizer.pretrained_model,
-            sent_level_BERT_config=cfg.model.sent_level_BERT_config,
-            optim=cfg.optim,
-            _recursive_=False,
-        )
-
-    early_stop_callback = hydra.utils.instantiate(
-        cfg.early_stopping,
-    )
-
-    checkpoint_callback = pl.callbacks.ModelCheckpoint(
-        #dirpath=checkpoints_dir,
-        filename='{epoch}',
-        verbose=True,
-        monitor='val_loss',
-        mode='min',
-        save_top_k=1
-    )
-
-    #TODO: sentence level がGRUのバージョンと分ける。
-    # elif cfg.model.name=='HierBERT' and
-
-    tb_logger = pl_loggers.TensorBoardLogger(".", "", "", log_graph=True, default_hp_metric=False)
-
-    trainer = pl.Trainer(
-        **OmegaConf.to_container(cfg.trainer),
-        callbacks=[checkpoint_callback, early_stop_callback],
-        logger=tb_logger,
-        plugins=DDPPlugin()
-    )
-
     try:
+        gmail_sender = Gmailsender(subject=f"Execution end notification (model:{cfg.model.name}, data:{cfg.data.name})")
+
+        pl.seed_everything(1234)
+        logger.info("\n" + OmegaConf.to_yaml(cfg))
+
+        if cfg.model.name == 'HAN':
+            tokenizer = hydra.utils.instantiate(
+                cfg.model.tokenizer,
+                data_dir=cfg.data.dir,
+            )
+
+            data_module = hydra.utils.instantiate(
+                cfg.model.data_module,
+                data_dir=cfg.data.dir,
+                tokenizer=tokenizer,
+            )
+
+            model = hydra.utils.instantiate(
+                cfg.model.general,
+                optim=cfg.optim,
+                embedding_matrix=tokenizer.embedding_matrix,
+                _recursive_=False,
+            )
+
+        elif cfg.model.name=='HierBERT' or cfg.model.name=='HierRoBERT':
+            # TODO: 色々書く。
+            data_module = hydra.utils.instantiate(
+                cfg.model.data_module,
+                data_dir=cfg.data.dir,
+                tokenizer=cfg.model.tokenizer,
+                _recursive_=False,
+            )
+
+            model = hydra.utils.instantiate(
+                cfg.model.model,
+                pretrained_model=cfg.model.tokenizer.pretrained_model,
+                sent_level_BERT_config=cfg.model.sent_level_BERT_config,
+                optim=cfg.optim,
+                _recursive_=False,
+            )
+
+        early_stop_callback = hydra.utils.instantiate(
+            cfg.early_stopping,
+        )
+
+        checkpoint_callback = pl.callbacks.ModelCheckpoint(
+            #dirpath=checkpoints_dir,
+            filename='{epoch}',
+            verbose=True,
+            monitor='val_loss',
+            mode='min',
+            save_top_k=1
+        )
+
+        tb_logger = pl_loggers.TensorBoardLogger(".", "", "", log_graph=True, default_hp_metric=False)
+
+        trainer = pl.Trainer(
+            **OmegaConf.to_container(cfg.trainer),
+            callbacks=[checkpoint_callback, early_stop_callback],
+            logger=tb_logger,
+            plugins=DDPPlugin()
+        )
+
         trainer.fit(model=model, datamodule=data_module)
         trainer.test(ckpt_path=checkpoint_callback.best_model_path)
     except Exception as e:
