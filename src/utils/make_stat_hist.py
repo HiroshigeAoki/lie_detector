e8d5496969470f26c57bc9cee83f3ea950128d43
Aoki Hiroshige <aoki.hiroshige.18@shizuoka.ac.jp>
2021-10-29 19:18:21 +0900

    fixed

--- src/utils/make_stat_hist.py
+++ src/utils/make_stat_hist.py
@@ -12,26 +12,25 @@ def count_subword(df, tokenizer) -> int:
         for _, utter in row.iteritems():
             tokenized = tokenizer.tokenize(utter)
             count.append(len(tokenized))
-    return sum(count)
+    return count
 
 
 def main():
     sender = Gmailsender()
-    sender.send("処理開始")
     train = pd.read_pickle("../../data/nested/train.pkl")
     valid = pd.read_pickle("../../data/nested/valid.pkl")
     test = pd.read_pickle("../../data/nested/test.pkl")
 
     all_num_utter = pd.concat((train.loc[:,"num_utters"], valid.loc[:,"num_utters"], test.loc[:,"num_utters"]), axis=0, ignore_index=True)
-    fig_utter = all_num_utter.hist(bins=80, grid=True , xlabelsize=8, ylabelsize=8).set_xticks(ticks=list(range(0,180,10)))
+    fig_utter = all_num_utter.hist(bins=80, grid=True , xlabelsize=8, ylabelsize=8).set_xticks(ticks=list(range(0,210,10)))
     fig_utter[0].figure.savefig("../../data/nested/num_utter_hist.png")
 
     tokenizer = BertJapaneseTokenizer.from_pretrained('cl-tohoku/bert-base-japanese-v2', additional_special_tokens=['<person>'])
     all_utter = pd.concat((train.loc[:,"nested_utters"], valid.loc[:,"nested_utters"], test.loc[:,"nested_utters"]), axis=0, ignore_index=True)
     pandarallel.initialize()
-    subword_count = all_utter.parallel_apply(count_subword, tokenizer=tokenizer)
-    fig_mor = subword_count.hist(bins=80, grid=True , xlabelsize=8, ylabelsize=8) #.set_xticks(ticks=list(range(0,180,100)))
-    fig_mor[0].figure.savefig("../../data/nested/num_sub_hist.png")
+    subword_count = all_utter.parallel_apply(count_subword, tokenizer=tokenizer).explode(ignore_index=True)
+    fig_sub = subword_count.hist(bins=150, grid=True , xlabelsize=8, ylabelsize=8).set_xticks(ticks=list(range(0,600,50)))
+    fig_sub[0].figure.savefig("../../data/nested/num_sub_hist.png")
     sender.send("histogram作成終了。")
 
 if __name__ == "__main__":
