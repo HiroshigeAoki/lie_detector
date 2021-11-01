9194ea4f069e7a53aea40283d98984059fd665b6
Aoki Hiroshige <aoki.hiroshige.18@shizuoka.ac.jp>
2021-11-01 15:51:40 +0900

    add index page

--- main.py
+++ main.py
@@ -94,8 +94,8 @@ def main(cfg: DictConfig) -> None:
             trainer.test(model=test_model, datamodule=data_module)
 
         elif cfg.mode == 'plot_attention':
-            #ckpt_path = f'/disk/ssd14tb/haoki/Documents/vscode-workplaces/lie_detector/outputs/wereWolf/HAN/baseline/{cfg.name}/checkpoints/epoch={cfg.best_epoch}.ckpt'
-            ckpt_path = f'checkpoints/epoch={cfg.best_epoch}.ckpt'
+            ckpt_path = f'/disk/ssd14tb/haoki/Documents/vscode-workplaces/lie_detector/outputs/wereWolf/HAN/baseline/{cfg.name}/checkpoints/epoch={cfg.best_epoch}.ckpt'
+            #ckpt_path = f'checkpoints/epoch={cfg.best_epoch}.ckpt'
             predict_model = model.load_from_checkpoint(ckpt_path)
             outputs = trainer.predict(model=predict_model, datamodule=data_module)
             if cfg.model.name == 'HAN':
@@ -120,45 +120,61 @@ def main(cfg: DictConfig) -> None:
             def make_ploted_doc(i, input_ids, word_weights,  sent_weights ,prob, pred, label, kwargs):
                 doc = [list(map(lambda x: x.replace(' ', ''), tokenizer.batch_decode(ids.tolist()))) for ids in input_ids]
                 ploted_doc = plot_attentions(doc=doc, word_weights=word_weights, sent_weights=sent_weights, **kwargs)
-                table_of_contents = dict(TP=[], TN=[], FP=[], FN=[])
+                table_of_contents_list = []
                 if pred == label:
                     if label == 1:
                         save_path = f'ploted_attention/TP/DC:{prob[label] * 100:.2f}% No.{i}.html' # DV stands for Degree of Conviction
-                        table_of_contents['TP'].append(save_path.replace('ploted_attention/', './'))
+                        table_of_contents_list.extend(('TP', save_path.replace('ploted_attention/TP/', '')))
                     elif label == 0:
                         save_path = f'ploted_attention/TN/DC:{prob[label] * 100:.2f}% No.{i}.html'
-                        table_of_contents['TN'].append(save_path.replace('ploted_attention/', './'))
+                        table_of_contents_list.extend(('TN', save_path.replace('ploted_attention/TN/', '')))
                 elif pred != label:
                     if label == 1:
                         save_path = f'ploted_attention/FP/DC:{prob[pred] * 100:.2f}% No.{i}.html'
-                        table_of_contents['FP'].append(save_path.replace('ploted_attention/', './'))
+                        table_of_contents_list.extend(('FP', save_path.replace('ploted_attention/FP/', '')))
                     elif label == 0:
                         save_path = f'ploted_attention/FN/DC:{prob[pred] * 100:.2f}% No.{i}.html'
-                        table_of_contents['FN'].append(save_path.replace('ploted_attention/', './'))
+                        table_of_contents_list.extend(('FN', save_path.replace('ploted_attention/FN/', '')))
                 with open(save_path, 'w') as f:
                     f.write(ploted_doc)
-                return table_of_contents
+                return table_of_contents_list
 
             list_args = [(i, *args) for i, args in enumerate(zip(input_ids, word_attentions, sent_attentions, probs, preds, labels))]
 
             kwargs = dict(
-                threshold=0.01, word_cmap="Blues" , sent_cmap="Reds",
-                word_color_level=4, sent_color_level=35, size=3,
+                threshold=0.05, word_cmap="Blues" , sent_cmap="Reds",
+                word_color_level=3, sent_color_level=30, size=3,
                 ignore_tokens=ignore_tokens,
                 pad_token=pad_token,
             )
 
-            # TODO: indexのページを作成する。
-            table_of_contents = joblib.Parallel(n_jobs=-1)(
+            table_of_contents_list = joblib.Parallel(n_jobs=15, prefer='threads')(
                 joblib.delayed(make_ploted_doc)(
                     *args,
                     kwargs=kwargs,
                 ) for args in tqdm(list_args, desc='making ploted doc')
             )
+            template = '<td><a href="{}">{}</a></td>'
 
-            file = '<td><a href="{}">{}</a></td>'
+            table_of_contents = dict(TP=[], TN=[], FP=[], FN=[])
+            for tc in table_of_contents_list:
+                table_of_contents.get(tc[0]).append(tc[1])
+
+            par_link = [template.format(f'./{key}.html', key) for key in table_of_contents.keys()]
             with open('ploted_attention/index.html', 'w') as f:
-                f.write()
+                f.write('<ui>')
+                for link in par_link:
+                    f.write('<li>' + link + '</li>')
+                f.write('</ui>')
+
+            for key, file_names in table_of_contents.items():
+                file_names = sorted(file_names, reverse=True)
+                chi_link = [template.format(f'./{key}/{file_name}', file_name) for file_name in file_names]
+                with open(f'ploted_attention/{key}.html', 'w') as f:
+                    f.write('<ui>')
+                    for link in chi_link:
+                        f.write('<li>' + link + '</li>')
+                    f.write('</ui>')
 
         else:
             raise Exception(f'Mode:{cfg.mode} is invalid.')
