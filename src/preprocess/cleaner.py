81a1086218cb5c093bf71a1124984b4cb8d8f4c6
Aoki Hiroshige <aoki.hiroshige.18@shizuoka.ac.jp>
2021-11-01 15:52:21 +0900

    cleaned up

--- src/preprocess/cleaner.py
+++ src/preprocess/cleaner.py
@@ -1,8 +1,4 @@
 import re
-from typing import Tuple
-import torch
-import numpy as np
-import os, sys
 import neologdn
 import demoji
 
@@ -28,22 +24,4 @@ def replace_term(sent: str) -> str:
     names = "行商人 アルビン|アル(ビン)*|行商人|商人|村長 ヴァルター|そんちょ|村長|ヴァル(ター)*|仕立て屋 エルナ|エルナ*|エレナ|エ*ルナ|パン屋 オットー|オットー*|オト|パン屋|羊飼い カタリナ|カタ(リナ|りん)*|(カタ)*リナ|羊飼い|司書 クララ|クララ*|ク*ララ|司書|楽天家 ゲルト|ゲルト*|楽天家|神父 ジムゾン|ジム(ゾン)*|神父|負傷兵 シモン|シモン*|兵隊|負傷兵|ならず者 ディーター|Ｄ太|ディー*タ*ー*|ならず者|木こり トーマス|トー*マス*|トム|機関車トー○ス|肉妖精|木こり|旅人 ニコラス|スナフキン|薄緑|旅人|ニコ(ラス|ルン)*|村娘 パメラ|村娘|娘|パメラ*|シスター フリーデル|シスター|フリ(ーデル)*|リデル|尼|少年 ペーター|ピーター|ベーター|ペー太|ペタ|少年|ペーター|老人 モーリッツ|おじいちゃん|お爺さん|(モー)*爺|(モリ)*爺|翁|長老|老人|モーリッツ|農夫 ヤコブ|ヤコ(ブ|ビン|ぷー)*|やこびー|農夫|青年 ヨアヒム|ヨア(ヒム)*|よあひー|青年|少女 リーザ|リ(ーズ|ーザ|ザ)+|少女|宿屋の女主人 レジーナ|おばさん|マダム|レジ(ーナ)*|姐さん|姐御|宿屋の女主人|女将|小母様|レジーナ"
     pattern = re.compile(names)
     sent = re.sub(pattern, '<person>', sent)
-
     return sent
-
-
-class auto_exclude_sent():
-    def __init__(self):
-        #load model
-        self.model, self.tokenizer = load_model('sentence_classifier_RoBERTa')
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.model.to(self.device)
-        self.model.eval()
-        self.utter_type = {0: "CO", 1: "guard", 2: "inspect", 3: "other"}
-
-    def __call__(self, sent: str) -> Tuple[bool, str]:
-        encoded = self.tokenizer(sent, padding='max_length', max_length=128, truncation=True, return_tensors='pt')
-        encoded = encoded.to(self.device)
-        preds = self.model(encoded['input_ids'], encoded['attention_mask'])
-        result = np.argmax(preds.logits.cpu().detach().numpy())
-        return (result!=3, self.utter_type.get(result))
