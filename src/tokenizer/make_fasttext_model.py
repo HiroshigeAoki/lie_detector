b08512b1d6b6886abefc63a57df0ce81bbe149af
Aoki Hiroshige <aoki.hiroshige.18@shizuoka.ac.jp>
2021-12-23 00:07:48 +0900

    organized

--- src/tokenizer/make_fasttext_model.py
+++ src/tokenizer/make_fasttext_model.py
@@ -2,28 +2,35 @@ import fasttext
 import argparse
 import os, sys
 import shutil
-sys.path.append(os.pardir)
+from gensim.models import fasttext as gensim_fasttext
+print(sys.path)
+sys.path.append('./src/')
+print(sys.path)
 from utils.gmail_send import Gmailsender
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--dim", type=int, default=200)
-parser.add_argument("--data", type=str, default='nested')
-parser.add_argument("--tokenizer", type=str, default='mecab-wordpiece')
-args = parser.parse_args()
 
-model = fasttext.train_unsupervised(f'../../data/{args.data}/split-train-{args.tokenizer}.txt',
+def main(args):
+    model = fasttext.train_unsupervised(os.path.join(args.input_dir, f'split-train-{args.tokenizer}.txt'),
                                         model='skipgram',
                                         dim=args.dim,
                                         minCount=1)
 
-save_dir = f'{args.tokenizer}_vectors/dim_{args.dim}'
-shutil.rmtree(save_dir)
-os.makedirs(save_dir, exist_ok=True)
-model.save_model(f"{save_dir}/model_fasttext.bin")
+    save_dir = os.path.join(args.save_dir, f'{args.tokenizer}_vectors/dim_{args.dim}')
+    shutil.rmtree(save_dir)
+    os.makedirs(save_dir, exist_ok=True)
+    model.save_model(f"{save_dir}/model_fasttext.bin")
 
-from gensim.models import fasttext
-model = fasttext.load_facebook_vectors(f"{save_dir}/model_fasttext.bin")
-model.save_word2vec_format(f"{save_dir}/model_fasttext.vec")
+    model = gensim_fasttext.load_facebook_vectors(f"{save_dir}/model_fasttext.bin")
+    model.save_word2vec_format(f"{save_dir}/model_fasttext.vec")
 
-sender = Gmailsender()
-sender.send(f"fasttext(dim={args.dim})訓練終わり。")
+    sender = Gmailsender()
+    sender.send(f"fasttext(dim={args.dim})訓練終わり。")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dim", type=int, default=200)
+    parser.add_argument("--input_dir", type=str, default='data/nested')
+    parser.add_argument("--save_dir", type=str, default='model')
+    parser.add_argument("--tokenizer", type=str, default='mecab-wordpiece')
+    args = parser.parse_args()
+    main(args)
--- src/tokenizer/make_sp_model.py
+++ src/tokenizer/make_sp_model.py
@@ -1,36 +1,33 @@
 import sentencepiece as spm
-from pathlib import Path
 import argparse
 
-import os, sys
-sys.path.append(os.pardir)
-from utils.unix_command import mkdir
+import os
 
 
 def main(args):
-    save_dir = Path('./flat') if args.flat else Path('./nest')
-    mkdir(save_dir)
+    os.makedirs(args.save_dir, exist_ok=True)
 
     sp = spm.SentencePieceProcessor()
-    spm.SentencePieceTrainer.Train(f"--input={save_dir}train.txt"
-                                "--user_defined_symbols=<br> "
-                                "--user_defined_symbols=<EOS> "
-                                "--pad_id=3 "
-                                "--model_prefix=wereWolf "
-                                f"--model_dir={save_dir}"
+    spm.SentencePieceTrainer.Train(f"--input={os.path.join(args.input_dir, 'bbs.txt')} "
+                                "--user_defined_symbols=<person> "
+                                f"--model_prefix={os.path.join(args.save_dir, 'werewolf')} "
                                 "--vocab_size=32000 "
-                                "--model_type=bpe")
+                                "--model_type=bpe "
+                                "--control_symbols=[PAD],[CLS],[SEP],[MASK] "
+                                )
 
     sp = spm.SentencePieceProcessor()
-    sp.load(save_dir / 'wereWolf.model')
-    with open(save_dir / "train.txt") as f:
-        with open(save_dir / 'split_train.txt', "w") as outfile:
+    sp.load(os.path.join(args.save_dir, 'werewolf.model'))
+    with open(os.path.join(args.input_dir, 'bbs.txt')) as f:
+        with open(os.path.join(args.input_dir, 'split-train-sp.txt'), "w") as outfile:
             for line in f:
                 outfile.writelines(" ".join(sp.Encode(input=line, out_type=str))+"\n")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--flat", action='store_true')
+    parser.add_argument("--input_dir", type=str, default='data/nested')
+    parser.add_argument("--save_dir", type=str, default='model/sp')
+
     args = parser.parse_args()
     main(args)
