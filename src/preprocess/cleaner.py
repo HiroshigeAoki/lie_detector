953f64ee47b9096853d13ad860cf2e47e12554cf
Aoki Hiroshige <aoki.hiroshige.18@shizuoka.ac.jp>
2021-12-28 21:01:52 +0900

    add new the way to make more clean datasets.

--- src/preprocess/cleaner.py
+++ src/preprocess/cleaner.py
@@ -1,13 +1,16 @@
 import re
 import neologdn
 import demoji
+import regex
 
 
 def clean_sent(sent: str) -> str:
-    sent = neologdn.normalize(sent) # 表記揺れを無くす
+    sent = neologdn.normalize(sent, repeat=3) # 表記揺れを無くす
+    sent = sent.lower()
     sent = demoji.replace(string=sent, repl="") # 絵文字削除
     sent = re.sub((r'[\(@][^あ-ん\u30A1-\u30F4\u2E80-\u2FDF\u3005-\u3007\u3400-\u4DBF\u4E00-\u9FFF\uF900-\uFAFF\U00020000-\U0002EBEF]+?\)'), '', sent) # 顔文字削除
-    sent = sent.replace('^^', '')
+    sent = re.sub(r"[m|c|u|o|つ|っ|ノ|シ|ヽ|人|⊂|＜|＞|<|>|≦|≧|≲|≳|≪|≫|《|》|\^|⌍|♪|＾|⌒|@|σ|α|β|∂|≡|=|-|ー|~|～|ﾟ|・|·|་|\*|＊|'|`|｀|´|❛|;|:|!|\?|,|\.|\(|\)|\||｜|￥|\\|\/|＼|／]*(Д|д|ω|ᴗ|∇|_ _|σ|α|β)[m|c|u|o|つ|っ|ノ|シ|ヽ|人|⊂|＜|＞|<|>|≦|≧|≲|≳|≪|≫|《|》|\^|⌍|♪|＾|⌒|@|σ|α|β|∂|≡|=|-|ー|~|～|ﾟ|・|·|་|\*|＊|'|`|｀|´|❛|;|:|!|\?|,|\.|\(|\)|\||｜|￥|\\|\/|＼|／]*", '', sent) # 顔文字削除
+    sent = re.sub(r'\^\^|orz', '', sent)
     sent = re.sub(r'[\t\n\r]', '', sent) # remove tab, newline
     sent = re.sub(r'[>]+[0-9]+', '', sent) # remove res anker
     sent = re.sub(r'[@][0-9]+', '', sent) # remove res anker
@@ -17,11 +20,101 @@ def clean_sent(sent: str) -> str:
     sent = re.sub(r'[0-9,]+[0-9]+', '0', sent) # numbers to 0
     sent = re.sub(r'[0-9]+\.?[0-9]*', '0', sent) # decimal to 0
     sent = re.sub(r'[【】\[\]]', '', sent)
+    sent = re.sub(r'0:0', '0時', sent)
+    sent = re.sub(r'0d', '0日', sent)
+    sent = re.sub(r'\|0\|0|0-0', '0', sent)
     return sent.strip()
 
 
 def replace_term(sent: str) -> str:
-    names = "行商人 アルビン|アル(ビン)*|行商人|商人|村長 ヴァルター|そんちょ|村長|ヴァル(ター)*|仕立て屋 エルナ|エルナ*|エレナ|エ*ルナ|パン屋 オットー|オットー*|オト|パン屋|羊飼い カタリナ|カタ(リナ|りん)*|(カタ)*リナ|羊飼い|司書 クララ|クララ*|ク*ララ|司書|楽天家 ゲルト|ゲルト*|楽天家|神父 ジムゾン|ジム(ゾン)*|神父|負傷兵 シモン|シモン*|兵隊|負傷兵|ならず者 ディーター|Ｄ太|ディー*タ*ー*|ならず者|木こり トーマス|トー*マス*|トム|機関車トー○ス|肉妖精|木こり|旅人 ニコラス|スナフキン|薄緑|旅人|ニコ(ラス|ルン)*|村娘 パメラ|村娘|娘|パメラ*|シスター フリーデル|シスター|フリ(ーデル)*|リデル|尼|少年 ペーター|ピーター|ベーター|ペー太|ペタ|少年|ペーター|老人 モーリッツ|おじいちゃん|お爺さん|(モー)*爺|(モリ)*爺|翁|長老|老人|モーリッツ|農夫 ヤコブ|ヤコ(ブ|ビン|ぷー)*|やこびー|農夫|青年 ヨアヒム|ヨア(ヒム)*|よあひー|青年|少女 リーザ|リ(ーズ|ーザ|ザ)+|少女|宿屋の女主人 レジーナ|おばさん|マダム|レジ(ーナ)*|姐さん|姐御|宿屋の女主人|女将|小母様|レジーナ"
-    pattern = re.compile(names)
-    sent = re.sub(pattern, '<person>', sent)
+    names = [
+        "(?V1)行商人 アルビン|アルビン",
+        "行商人|商人",
+        "村長 ヴァルター|そんちょ|村長|ヴァル(ター)*|ヴェル",
+        "仕立て屋 エルナ|エルナ|エルゥ|エレナ",
+        "パン屋 オットー|オットー*|パン屋",
+        "羊飼い カタリナ|カタリナ|カタりん|羊飼い",
+        "司書 クララ|クララ|クラりん|司書|クララん",
+        "楽天家 ゲルト|楽天家",
+        "神父 ジムゾン|ジムゾ|ジム=ゾン|ジム!ゾン!|神父",
+        "負傷兵 シモン|シモン*|兵隊|負傷兵",
+        "ならず者 ディーター|Ｄ太|ディーター|ディッタン|ディダ|ならず者",
+        "木こり トーマス|トー*マス*|トム|機関車トー○ス|肉妖精|木こり",
+        "旅人 ニコラス|スナフキン|薄緑|旅人|ニコ(ラス|ルン)+|ニコラ",
+        "村娘 パメラ|村娘|娘|パメラ*",
+        "シスター フリーデル|シスター|リデル",
+        "少年 ペーター|ペータ(ー)*|ピーター|ベーター|ペー太|ぺーた(ー|ん)*|ぺー(君|にゃん)|ペタ|少年|ペーター",
+        "老人 モーリッツ|おじいちゃん|じーさん|じいちゃん|じじい|お爺さん|もーじ[い|ぃ]|長老|老人|モーリッツ|モーリツ|モっちん",
+        "農夫 ヤコブ|ヤコ(ブ|ビン|ぷー)*|やこびー|農夫", "青年 ヨアヒム|ヨア(ヒム)*|ヨアヒ|よあひー|よあひむ|青年",
+        "少女 リーザ|リ(ーズ|ーザ|ザ)+|少女",
+        "宿屋の女主人 レジーナ|おばさん|マダム|レジ(ーナ)*|レジナ|姐さん|姐御|宿屋の女主人|宿屋のおかみ|女将|小母様|レジーナ"
+    ]
+    name_char = "楽|村|長|老|爺|翁|神|木|樵|旅|者|年|尼|少|妙|女|好|行|商|羊|屋|青|娘|農|宿|姐|修|尼|仕|服|司|書|兵|傷|負"
+    nick_names = '|'.join([
+        "エル",
+        "ルナ",
+        "オト",
+        "カタ", "リナ",
+        "クラ", "ララ",
+        "ゲル",
+        "ジム",
+        "ディタ",
+        "ディ",
+        "フリ",
+        "モリ", "モー",
+    ])
+    # TODO: モーリツ以降もチェック。
+
+    particles = ["は","も","を","が","か","で","と","に","の","や","へ"]
+    honorific_titles = [
+        "姉", "ね[え|ぇ]", "お姉", "おね[え|ぇ]", "お嬢"
+        "兄", "に[い|ぃ]", "お兄", "おに[え|ぇ]",
+        "おじいちゃん", "爺", "翁", "じ[い|ぃ]",
+        "さん","くん", "君", "ちゃん", "坊", "子",
+        "様", "さま", "殿", "どの",
+        "どん", "さぁ", "はん",
+        "ち", "っち", "ぴょん", "ポン", "にゃん", "わん", "たん", "らん", "りん", "猫ちゃん", "ワンちゃん"
+    ]
+    person_particles = '|'.join([f"<person>{p}" for p in particles])
+    person_honorific_titles = '|'.join([f"<person>{h}" for h in honorific_titles])
+    particles = '|'.join(particles)
+    honorific_titles = '|'.join(honorific_titles)
+    role_names = "狼|人狼|羊|灰|偽|白|黒|真|占|狂|狩|霊|共"
+    symbols = "→|-|⇒|or|=|>|≧|×|「|:|。|\(|\)|・|0"
+
+    reg1 = regex.compile('|'.join(names))
+    sent = regex.sub(reg1, '<person>', sent)
+
+    ignore_prefix = "共有|能力|霊能|微|進|続|奇|この|確定|うちの|勝|色々|流|隣"
+    ignore_suffix = "い|う|な|し|性|っ|[き-け]|騙り|の開始|に泊|器|品"
+    # reg2 = regex.compile(rf"(?V1)(?<!({ignore_prefix}))({name_char}|{nick_name})(?=(どっち|的には|0|のように|視点|確定|確白|確黒|希望|両狼|確認|考察|襲撃|護衛|直吊|吊|即吊|ライン|連打|不在|パンダ|ぱんだ|墓|ごめん|ありがとう|食[っ|べ|う]|{symbols}|<person>|{role_names}|{name_char}|{particles}|{honorific_titles}|のco|の判定|の発言|の占い|の内心|[{particles}{symbols}][{name_char}|{role_names}]))")
+    reg2 = regex.compile(rf"(?V1)(?<!({ignore_prefix}))({name_char}|{nick_names})(?=(どっち|的には|のように|視点|確定|確白|確黒|希望|両狼|確認|考察|襲撃|護衛|直吊|吊|即吊|ライン|連打|不在|パンダ|ぱんだ|墓|ごめん|ありがとう|食[っ|べ|う]|{symbols}|<person>|{role_names}|{name_char}|{particles}|{honorific_titles}|のco|の判定|の発言|の占い|の内心|([{particles}{symbols}]|{nick_names})[{name_char}|{role_names}]))")
+    # reg3 = regex.compile(rf"(?V1)(?<=([{name_char}{role_names}][{particles}|{honorific_titles}]|<person>|{person_particles}|{person_honorific_titles}|{symbols}|{role_names}|{name_char}|希望出し))({name_char}|{nick_name})(?!({ignore_suffix}))")
+    reg3 = regex.compile(rf"(?V1)(?<=((([{name_char}{role_names}]|{nick_names})[{particles}|{honorific_titles}]|<person>|{person_particles}|{person_honorific_titles}|{symbols}|{role_names}|{name_char}|希望出し))({name_char}|{nick_names})(?!({ignore_suffix}))")
+    # TODO names/nickname particles/honorific_titlesでちゃんと変換されるかチェック。
+
+    sent = regex.sub(r'旅行(?=(に|へ|でもいかない|でもいかない|行く|いく))', '&旅&行&', sent)
+    sent = regex.sub(r'負傷(?=(した|して|がひどい|こそ|具合))', '&負&傷&', sent)
+    sent = regex.sub(r'(?=(足|手|体))負傷', '&負&傷&', sent)
+    sent = regex.sub(r'宿屋(?=(に|の))', '&宿&屋&', sent)
+    sent = regex.sub(r'村人(?=(|なら|は|の|を|))', '&村&人&', sent)
+    sent = regex.sub(r'村(?=(|のために|として|[は|の]負け|を滅ぼ|[は|の]勝))', '@村@', sent)
+
+    sent = regex.sub(reg2, '<person>', sent)
+    sent = regex.sub(reg3, '<person>', sent)
+
+    sent = re.sub('&旅&行&', '旅行', sent)
+    sent = re.sub('&負&傷&', '負傷', sent)
+    sent = re.sub('&宿&屋&', '宿屋', sent)
+    sent = re.sub('&村&人&', '村人', sent)
+    sent = re.sub('@村@', '村', sent)
+
+    sent = re.sub(r'灰{2,}', '灰', sent)
+    sent = re.sub(r'白{2,}', '白', sent)
+    sent = re.sub(r'黒{2,}', '黒', sent)
+
+    # sent = regex.sub(r'リ<person>|<person>ナ|<person>ブ|お<person>(?=(ちゃん))|モリお<person>', '<person>', sent)
+    #sent = re.sub(r'\(*<person>\)', '<person>', sent)
+    #sent = re.sub(r'(<person>){2,}', '<person>', sent)
+    #sent = regex.sub(r'(<person>|<|>)+', '<person>', sent)
     return sent
---
+++ src/preprocess/make_user_separated_hierarchical_data.py
@@ -0,0 +1,243 @@
+import json
+import glob
+import pickle
+from tqdm import trange, tqdm
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from transformers import BertJapaneseTokenizer
+import shutil
+import argparse
+import os, sys
+import joblib
+sys.path.append('./src/')
+from preprocess.cleaner import clean_sent, replace_term
+from utils.gmail_send import Gmailsender
+from preprocess.custom_mecab_tagger import CustomMeCabTagger
+
+
+def extract(filePaths, save_dir, kwargs):
+    """ファイルから必要なところを取り出してリストにまとめます"""
+    nested_utterances = []
+    labels = []
+    users = []
+    deleted = []
+
+    outputs = joblib.Parallel(n_jobs=-1)(
+        joblib.delayed(extract_loop)(
+            filepath,
+            kwargs
+        ) for filepath in tqdm(filePaths, desc='extracting...')
+    )
+
+    for _nested_utterances, _labels, _users, _deleted in outputs:
+        nested_utterances += _nested_utterances
+        labels += _labels
+        users += _users
+        deleted += _deleted
+
+    grouped = pd.DataFrame(
+        dict(
+            nested_utterances=nested_utterances,
+            labels=labels,
+            users=users
+        )
+    ).groupby('users')
+    groups = [grouped.get_group(x) for x in grouped.groups]
+
+    print('split data')
+    i = 0
+    train, train_size = [groups[i]], len(groups[i])
+    while train_size < len(labels) * 0.8:
+        i += 1
+        train_size += len(groups[i])
+        train.append(groups[i])
+
+    valid, valid_size = [groups[i]], len(groups[i])
+    while valid_size < len(labels) * 0.1:
+        i += 1
+        valid_size += len(groups[i])
+        valid.append(groups[i])
+
+    test, test_size = [groups[i]], len(groups[i])
+    while test_size < len(labels)* 0.1:
+        i += 1
+        test_size += len(groups[i])
+        test.append(groups[i])
+
+    train.extend(groups[i+1:])
+
+    train, valid, test = pd.concat(train), pd.concat(valid), pd.concat(test)
+    X_train, y_train, users_train = train['nested_utterances'].tolist(), train['labels'].tolist(), train['users'].tolist()
+    X_valid, y_valid, users_valid = valid['nested_utterances'].tolist(), valid['labels'].tolist(), valid['users'].tolist()
+    X_test, y_test, users_test = test['nested_utterances'].tolist(), test['labels'].tolist(), test['users'].tolist()
+
+    train_for_tapt = []
+    for train in X_train:
+        train_for_tapt.extend(train)
+        train_for_tapt.append("")
+
+    with open(f'{save_dir}/bbs.txt', 'w') as f: # for tapt pretraining of RoBERTa.
+        for utterance in tqdm(train_for_tapt + deleted, desc="making bbs.txt"):
+            f.write(utterance + '\n')
+
+    tokenizer = CustomMeCabTagger("-O wakati -d /usr/local/lib/mecab/dic/mecab-ipadic-neologd -r /home/haoki/Documents/vscode-workplaces/lie_detector/src/tokenizer/mecab_userdic/mecabrc")
+    make_split_train(save_dir, X_train, tokenizer, file_name='split-train-mecab.txt') # mecab version
+
+    tokenizer = BertJapaneseTokenizer.from_pretrained('cl-tohoku/bert-large-japanese', additional_special_tokens=['<person>'])
+    make_split_train(save_dir, X_train, tokenizer, file_name='split-train-mecab-wordpiece.txt') # mecab wordpiece version
+
+    print("duplicating werewolf dataset")
+    X_train, y_train, users_train = duplicate_werewolves(X_train, y_train, users_train)
+    X_valid, y_valid, users_valid = duplicate_werewolves(X_valid, y_valid, users_valid)
+    X_test, y_test, users_test = duplicate_werewolves(X_test, y_test, users_test)
+
+    for _nested_utterances, _labels, _users, type in zip([X_train, X_valid, X_test], [y_train, y_valid, y_test], [users_train, users_valid, users_test], ['train', 'valid', 'test']):
+        X_dfs, num_utters = [], []
+        with trange(len(_nested_utterances), desc=f"pickling({type})...") as t:
+            for _, _utterances in zip(t, _nested_utterances):
+                X_df = pd.DataFrame({'raw_nested_utters': _utterances})
+                X_dfs.append(X_df)
+                num_utters.append(len(_utterances))
+        df = pd.DataFrame({'nested_utters': X_dfs, 'num_utters': num_utters, 'labels': _labels, 'users': _users})
+
+        with open(f'{save_dir}/{type}.pkl', 'wb') as f:
+            pickle.dump(df,  f, protocol=5)
+
+
+def extract_loop(filePath, kwargs):
+    deleted = []
+    delete_nested_utterances = []
+    participants = {}
+    users_dict = {}
+
+    with open(filePath, encoding='utf-8') as f:
+        data = json.load(f)
+        p = data.get('participants')
+        u = data.get('users')
+        participants.update(p)
+        users_dict.update(u)
+
+    """processing each day"""
+    days = data['days'][1:-1] # exclude prologue and epilogue
+    epilogue = data['days'][0]
+    prologue = data['days'][-1]
+
+    nested_utterances, labels, users, _deleted = preprocess(days=days, participants=participants, users_dict=users_dict, **kwargs)
+    deleted.extend(_deleted)
+    _delete_nested_utterances, _, _, _deleted = preprocess(days=[epilogue, prologue], participants=participants, users_dict=users_dict, **kwargs)
+    for utterances in _delete_nested_utterances:
+        delete_nested_utterances.extend(utterances)
+        delete_nested_utterances.append("")
+    deleted.extend(delete_nested_utterances + deleted)
+
+    return nested_utterances, labels, users, deleted
+
+
+def preprocess(days, participants, users_dict, role2label , used_role, min_len_char, min_num_utter):
+    nested_utterances = [] #(user_num, utterance_num)
+    labels = []
+    users = []
+    deleted = []
+    """Aggregate all utterances of each player, respectively."""
+    for participant in participants.keys():
+        if participant == '楽天家 ゲルト': # exclude a bot player.
+            continue
+        participant_role = participants[participant]
+        if participant_role not in used_role:
+            continue
+        _nested_utterance = []
+        _deleted = []
+        for i, day in enumerate(days):
+            for utterance_inf in day['utterances']:
+                if utterance_inf.get('subject') == participant:
+                    utterance = clean_sent(utterance_inf['utterance'])
+                    utterance = replace_term(utterance)
+                    if len(utterance) <= min_len_char:
+                        continue
+                    if utterance_inf.get('u_type') == 'say':
+                        _nested_utterance.append(utterance)
+                    else:
+                        _deleted.append(utterance)
+        _nested_utterance = sorted(set(_nested_utterance), key=_nested_utterance.index) # remove duplications
+        _deleted = sorted(set(_deleted), key=_deleted.index)
+        _deleted.append("")
+        deleted.extend(_deleted)
+        if len(_nested_utterance) > min_num_utter:
+            nested_utterances.append(_nested_utterance)
+            labels.append(role2label[participant_role])
+            users.extend(get_keys_from_value(users_dict, participant))
+
+    return nested_utterances, labels, users, deleted
+
+
+def get_keys_from_value(d, val):
+    return [k for k, v in d.items() if v == val]
+
+
+def duplicate_werewolves(nested_utterances, labels, users):
+    """oversample werewolf class and change ratio 50:50
+
+    Args:
+        nested_utterances (list): [description]
+        labels (list): [description]
+
+    Returns:
+        nested_utterances (list): [description]
+        labels (list): [description]
+    """
+    werewolf_indices = [i for i, label in enumerate(labels) if label == 1]
+    civil_num = len(labels) - len(werewolf_indices)
+    difference = civil_num - len(werewolf_indices)
+
+    for i in range(difference - len(werewolf_indices)):
+        werewolf_indices.append(werewolf_indices[i])
+
+    for i in werewolf_indices:
+        nested_utterances.append(nested_utterances[i])
+        labels.append(1)
+        users.append(users[i])
+
+    return nested_utterances, labels, users
+
+
+def make_split_train(save_dir, X_train , tokenizer, file_name):
+    """save split train data for fasttext training"""
+    with open(f'{save_dir}/{file_name}', 'w') as f:
+        for utterance in tqdm(sum(X_train, []), desc=f"making {file_name}"):
+            f.write(' '.join(tokenizer.tokenize(utterance + '\n')))
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--sample", action="store_true")
+
+    args = parser.parse_args()
+
+    files = sorted(glob.glob("../../corpus/BBSjsons/*/*.json"))  # 7249 files
+
+    save_dir = f"data/nested_separate_users"
+
+    if args.sample:
+        files = files[:100]
+        save_dir = save_dir.replace("nested", "nested_sample")
+
+    os.makedirs(save_dir, exist_ok=True)
+    shutil.rmtree(save_dir)
+    os.mkdir(save_dir)
+
+    kwargs = dict(
+        role2label={"人狼": 1, "狂人": 1, "村人": 0, "占い師": 0, "霊能者": 0, "狩人": 0, "共有者": 0, "ハムスター人間": 0},
+        used_role=["人狼", "狂人", "村人", "占い師", "霊能者", "狩人"],
+        min_len_char=10,
+        min_num_utter=10
+    )
+
+    extract(files, save_dir, kwargs)
+
+    print('done!')
+    if not args.sample:
+        gmail_sender = Gmailsender(subject="実行終了通知")
+        gmail_sender.send(body="mkNested.py終了")
+
+if __name__ == '__main__':
+    main()
